1c4bbc9444249b9919d39314f7ebfaa3c4e87fde
natsumiko000 <k.natsu5454@gmail.com>
2020-03-17 23:03:25 +0900

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '71488e405a0db08e8c0a34a9dc174287c364ba01ec89f63cb16cb70f8d3d597279f96b777a31ef08d4bd65292bb98006f6501f86090b11a5089bfec31578f89f'
